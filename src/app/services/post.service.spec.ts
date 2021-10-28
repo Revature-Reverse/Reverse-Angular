f38d6dadece6e4d30a3159f3eaa230ac9c5c481a
Timothy Harper <leeharperdev@gmail.com>
2021-10-28 15:37:18 -0500

    Update PostService
    Updated: PostService Angular Service.
    -> Implemented the savePost(post : any) method, which will be used for saving posts.
    -> Implemented a method for testing the savePost() method.

--- src/app/services/post.service.spec.ts
+++ src/app/services/post.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 
 import { PostService } from './post.service';
 
@@ -10,7 +11,34 @@ describe('PostService', () => {
     service = TestBed.inject(PostService);
   });
 
+  /**
+   *
+   * Test: (PostService) should be created.
+   * Purpose: Tests to see if the service can be loaded.
+   *
+   */
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  /**
+   *
+   * Test: (PostService) #savePost should save a post.
+   * Purpose: Tests to see if a post can be added through the service.
+   *
+   */
+  it("#savePost should save a post", () => {
+    let post : any = {
+      id: 4,
+      content: "<h3>I got it working!</h3><p>But now there are more issues.</p>",
+      user_id: 3,
+    }
+    let posts : any[] = [];
+
+    let oldPostsLength = service.posts.length;
+
+    service.savePost(post).subscribe(dbPosts => posts = dbPosts);
+
+    expect(posts.length).toEqual(oldPostsLength+1);
+  })
 });
--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import POSTS from '../POSTS';
+import { Observable } from "rxjs";
+import { of } from "rxjs"
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
 
-  constructor() { }
+  posts : any[] = POSTS;
+
+  constructor() {
+
+  }
+
+  /***
+   *
+   * Function: PostService.savePost(post : any).
+   * Purpose: Saves a given post to data storage.
+   * Precondition: A valid data storage is set up.
+   * Postcondition: The post is saved to data storage.
+   *
+   * @param post The post object that is being saved.
+   *
+   */
+  savePost(post : any) : Observable<any> {
+    this.posts.push(post);
+
+    return of(this.posts);
+  }
 }
