8318f9c44257478e0ef0bdcdc93f3ce03698703f
Jerry Zheng <jiaruizheng0714@gmail.com>
2021-10-28 14:04:57 -0400

    added components: login-register,post,user,add-post, and set up routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginRegisterComponent } from './components/login-register/login-register.component';
+import { UserComponent } from './components/user/user.component';
+import { PostComponent } from './components/post/post.component';
+import { AddPostComponent } from './components/add-post/add-post.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'login', component: LoginRegisterComponent },
+  { path: 'register', component: LoginRegisterComponent },
+  {
+    path: 'post/:id',
+    component: PostComponent,
+  },
+  {
+    path: 'user/:id',
+    component: UserComponent,
+  },
+  {
+    path: 'post/add',
+    component: AddPostComponent,
+  }
 
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
