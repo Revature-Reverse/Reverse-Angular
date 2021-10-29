2ac0c706cc7eda637639ede0871201e1f8817265
Paxton Plum <paxton.plum@revature.net>
2021-10-29 07:58:29 -0400

    tab image

--- src/app/components/login-register/login-register.component.ts
+++ src/app/components/login-register/login-register.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
-@Component({
-  selector: 'app-login-register',
+@Component
+({
+  selector: '/login-register',
   templateUrl: './login-register.component.html',
   styleUrls: ['./login-register.component.css']
 })
+
 export class LoginRegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor () {}
+
+
 
   ngOnInit(): void {
   }
