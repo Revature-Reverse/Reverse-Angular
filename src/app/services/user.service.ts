58915a56269f84e71e5c50523ea37d3d94b149f9
18vincent <18vincent@gmail.com>
2021-11-01 17:07:52 -0400

    part 1 attempt

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -33,3 +33,111 @@ arr.push({
   password2: "frontend dev 2"
 });
 arr.push(new User(1, 2, "frontend", "frontend dev", "frontend 2", "frontend dev 2"));
+
+type Props = RouteComponentProps<RouterProps>;
+
+type State = {
+  username: string,
+  password: string,
+  loading: boolean,
+  message: string
+};
+
+export default class Login extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.handleLogin = this.handleLogin.bind(this);
+
+    this.state = {
+      username: "",
+      password: "",
+      loading: false,
+      message: ""
+    };
+  }
+
+handleLogin(formValue: { username: string; password: string }) {
+  const { username, password } = formValue;
+
+  this.setState({
+    message: "",
+    loading: true
+  });
+
+
+  Login(username, password).then(
+    () => {
+      this.props.history.push("/profile");
+      window.location.reload();
+    },
+    error => {
+      const resMessage =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+
+      this.setState({
+        loading: false,
+        message: resMessage
+      });
+    }
+  );
+}
+
+  type State = {
+    username: string,
+    password: string,
+    successful: boolean,
+    message: string
+  };
+
+  export default class Register extends Component<Props, State> {
+    constructor(props: Props) {
+      super(props);
+      this.handleRegister = this.handleRegister.bind(this);
+
+      this.state = {
+        username: "",
+        password: "",
+        successful: false,
+        message: ""
+      };
+    }
+
+    handleRegister(formValue: { username: string; password: string }) {
+      const { username, password } = formValue;
+
+      this.setState({
+        message: "",
+        successful: false
+      });
+
+      register(
+        username,
+        password
+      ).then(
+        response => {
+          this.setState({
+            message: response.data.message,
+            successful: true
+          });
+        },
+        error => {
+          const resMessage =
+            (error.response &&
+              error.response.data &&
+              error.response.data.message) ||
+            error.message ||
+            error.toString();
+
+          this.setState({
+            successful: false,
+            message: resMessage
+          });
+        }
+      );
+    }
+
+sessionStorage.setItem('key', 'value');
