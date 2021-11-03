24b81f5ec2404c0e9af6160c53d8f926bf52da0f
Andrew Wolf <wolfpassion20@gmail.com>
2021-11-02 22:32:06 -0400

    populated related-users component template with top 3 posts in the POSTS.ts mock data

--- src/app/user-components/related-users/related-users.component.ts
+++ src/app/user-components/related-users/related-users.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import {Post} from "../../classes/Post";
+import POSTS  from "src/app/POSTS"
 
 @Component({
   selector: 'app-related-users',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RelatedUsersComponent implements OnInit {
 
+  @Input('posts')
+  posts: Post[] = []; 
+  postsMockData: Post[] = POSTS; 
+
+  populatePosts(): Post[]
+  {
+    this.posts = []; 
+    for (let i = 0; i < 3; i++)
+    {
+      this.posts.push(this.postsMockData[i]); 
+    }
+    return this.posts; 
+  }
+
+  
+
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
+
+
