5638db6c7b902ff832071b4a5fe7e7265c4eba79
Jerry Zheng <jiaruizheng0714@gmail.com>
2021-11-03 11:44:53 -0400

    moved code from post component to add post component

--- src/app/components/add-post/add-post.component.ts
+++ src/app/components/add-post/add-post.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Post} from "../../classes/Post";
+import {PostService} from "../../services/post.service";
+import POSTS from "../../POSTS";
 
 @Component({
   selector: 'app-add-post',
@@ -7,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddPostComponent implements OnInit {
 
-  constructor() { }
+  postform!:FormGroup;
+  post!:Post;
+
+  constructor(private postService:PostService,
+              private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.postform = this.formBuilder.group({
+      content : ["", [Validators.required]]
+    })
+  }
+  get f() { return this.postform.controls; }
+
+  onSubmit(){
+    this.post = {
+      id: POSTS.length+1,
+      content: this.f.content.value,
+      user_id: 1
+
+    }
+    this.postService.savePost(this.post).toPromise()
+      .then((res:any) =>{
+        console.log(res);
+      }, (error)=>{
+        alert("Please fill out all required fields.");
+      });
+
   }
 
 }
