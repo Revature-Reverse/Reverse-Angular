f563b88d9a9110260f20fd5f7ffbed578d1fa6bb
Timothy Harper <leeharperdev@gmail.com>
2021-11-04 13:04:20 -0500

    Update Post.ts & POSTS.ts
    Updated: Post.ts & POSTS.ts.
    -> Reimplemented the user_id field for the time being.

--- src/app/POSTS.ts
+++ src/app/POSTS.ts
@@ -60,4 +60,160 @@ export default [
     user_id: 2,
     time: new Date()
   },
+  {
+    id: 5,
+    title: 'Title 5',
+    content:
+      '<h3>Lorem ipsum.</h3><p>Mauris tortor eros, interdum at massa vitae, eleifend consequat justo. Donec ' +
+      'malesuada quam vel auctor vestibulum. Phasellus viverra, velit a malesuada suscipit, odio dui venenatis dui, ac ' +
+      'cursus purus nisi vel risus. Nullam convallis, dolor fringilla semper vulputate, massa nibh pellentesque orci, in ' +
+      'luctus elit tortor ut justo. Sed ac velit malesuada dolor iaculis facilisis. Nunc eget vehicula dolor, id dignissim t' +
+      'urpis. Etiam imperdiet vel risus sed rutrum. Donec ac viverra mi.</p>',
+    user_id: 2,
+    time: new Date()
+  },
+  {
+    id: 5,
+    title: 'Title 5',
+    content:
+      '<h3>Lorem ipsum.</h3><p>Mauris tortor eros, interdum at massa vitae, eleifend consequat justo. Donec ' +
+      'malesuada quam vel auctor vestibulum. Phasellus viverra, velit a malesuada suscipit, odio dui venenatis dui, ac ' +
+      'cursus purus nisi vel risus. Nullam convallis, dolor fringilla semper vulputate, massa nibh pellentesque orci, in ' +
+      'luctus elit tortor ut justo. Sed ac velit malesuada dolor iaculis facilisis. Nunc eget vehicula dolor, id dignissim t' +
+      'urpis. Etiam imperdiet vel risus sed rutrum. Donec ac viverra mi.</p>',
+    user_id: 2,
+    time: new Date()
+  },
+  {
+    id: 5,
+    title: 'Title 5',
+    content:
+      '<h3>Lorem ipsum.</h3><p>Mauris tortor eros, interdum at massa vitae, eleifend consequat justo. Donec ' +
+      'malesuada quam vel auctor vestibulum. Phasellus viverra, velit a malesuada suscipit, odio dui venenatis dui, ac ' +
+      'cursus purus nisi vel risus. Nullam convallis, dolor fringilla semper vulputate, massa nibh pellentesque orci, in ' +
+      'luctus elit tortor ut justo. Sed ac velit malesuada dolor iaculis facilisis. Nunc eget vehicula dolor, id dignissim t' +
+      'urpis. Etiam imperdiet vel risus sed rutrum. Donec ac viverra mi.</p>',
+    user_id: 2,
+    time: new Date()
+  },
+  {
+    id: 5,
+    title: 'Title 5',
+    content:
+      '<h3>Lorem ipsum.</h3><p>Mauris tortor eros, interdum at massa vitae, eleifend consequat justo. Donec ' +
+      'malesuada quam vel auctor vestibulum. Phasellus viverra, velit a malesuada suscipit, odio dui venenatis dui, ac ' +
+      'cursus purus nisi vel risus. Nullam convallis, dolor fringilla semper vulputate, massa nibh pellentesque orci, in ' +
+      'luctus elit tortor ut justo. Sed ac velit malesuada dolor iaculis facilisis. Nunc eget vehicula dolor, id dignissim t' +
+      'urpis. Etiam imperdiet vel risus sed rutrum. Donec ac viverra mi.</p>',
+    user_id: 2,
+    time: new Date()
+  },
+  {
+    id: 5,
+    title: 'Title 5',
+    content:
+      '<h3>Lorem ipsum.</h3><p>Mauris tortor eros, interdum at massa vitae, eleifend consequat justo. Donec ' +
+      'malesuada quam vel auctor vestibulum. Phasellus viverra, velit a malesuada suscipit, odio dui venenatis dui, ac ' +
+      'cursus purus nisi vel risus. Nullam convallis, dolor fringilla semper vulputate, massa nibh pellentesque orci, in ' +
+      'luctus elit tortor ut justo. Sed ac velit malesuada dolor iaculis facilisis. Nunc eget vehicula dolor, id dignissim t' +
+      'urpis. Etiam imperdiet vel risus sed rutrum. Donec ac viverra mi.</p>',
+    user_id: 2,
+    time: new Date()
+  },
+  {
+    id: 5,
+    title: 'Title 5',
+    content:
+      '<h3>Lorem ipsum.</h3><p>Mauris tortor eros, interdum at massa vitae, eleifend consequat justo. Donec ' +
+      'malesuada quam vel auctor vestibulum. Phasellus viverra, velit a malesuada suscipit, odio dui venenatis dui, ac ' +
+      'cursus purus nisi vel risus. Nullam convallis, dolor fringilla semper vulputate, massa nibh pellentesque orci, in ' +
+      'luctus elit tortor ut justo. Sed ac velit malesuada dolor iaculis facilisis. Nunc eget vehicula dolor, id dignissim t' +
+      'urpis. Etiam imperdiet vel risus sed rutrum. Donec ac viverra mi.</p>',
+    user_id: 2,
+    time: new Date()
+  },
+  {
+    id: 5,
+    title: 'Title 5',
+    content:
+      '<h3>Lorem ipsum.</h3><p>Mauris tortor eros, interdum at massa vitae, eleifend consequat justo. Donec ' +
+      'malesuada quam vel auctor vestibulum. Phasellus viverra, velit a malesuada suscipit, odio dui venenatis dui, ac ' +
+      'cursus purus nisi vel risus. Nullam convallis, dolor fringilla semper vulputate, massa nibh pellentesque orci, in ' +
+      'luctus elit tortor ut justo. Sed ac velit malesuada dolor iaculis facilisis. Nunc eget vehicula dolor, id dignissim t' +
+      'urpis. Etiam imperdiet vel risus sed rutrum. Donec ac viverra mi.</p>',
+    user_id: 2,
+    time: new Date()
+  },
+  {
+    id: 5,
+    title: 'Title 5',
+    content:
+      '<h3>Lorem ipsum.</h3><p>Mauris tortor eros, interdum at massa vitae, eleifend consequat justo. Donec ' +
+      'malesuada quam vel auctor vestibulum. Phasellus viverra, velit a malesuada suscipit, odio dui venenatis dui, ac ' +
+      'cursus purus nisi vel risus. Nullam convallis, dolor fringilla semper vulputate, massa nibh pellentesque orci, in ' +
+      'luctus elit tortor ut justo. Sed ac velit malesuada dolor iaculis facilisis. Nunc eget vehicula dolor, id dignissim t' +
+      'urpis. Etiam imperdiet vel risus sed rutrum. Donec ac viverra mi.</p>',
+    user_id: 2,
+    time: new Date()
+  },
+  {
+    id: 5,
+    title: 'Title 5',
+    content:
+      '<h3>Lorem ipsum.</h3><p>Mauris tortor eros, interdum at massa vitae, eleifend consequat justo. Donec ' +
+      'malesuada quam vel auctor vestibulum. Phasellus viverra, velit a malesuada suscipit, odio dui venenatis dui, ac ' +
+      'cursus purus nisi vel risus. Nullam convallis, dolor fringilla semper vulputate, massa nibh pellentesque orci, in ' +
+      'luctus elit tortor ut justo. Sed ac velit malesuada dolor iaculis facilisis. Nunc eget vehicula dolor, id dignissim t' +
+      'urpis. Etiam imperdiet vel risus sed rutrum. Donec ac viverra mi.</p>',
+    user_id: 2,
+    time: new Date()
+  },
+  {
+    id: 5,
+    title: 'Title 5',
+    content:
+      '<h3>Lorem ipsum.</h3><p>Mauris tortor eros, interdum at massa vitae, eleifend consequat justo. Donec ' +
+      'malesuada quam vel auctor vestibulum. Phasellus viverra, velit a malesuada suscipit, odio dui venenatis dui, ac ' +
+      'cursus purus nisi vel risus. Nullam convallis, dolor fringilla semper vulputate, massa nibh pellentesque orci, in ' +
+      'luctus elit tortor ut justo. Sed ac velit malesuada dolor iaculis facilisis. Nunc eget vehicula dolor, id dignissim t' +
+      'urpis. Etiam imperdiet vel risus sed rutrum. Donec ac viverra mi.</p>',
+    user_id: 2,
+    time: new Date()
+  },
+  {
+    id: 5,
+    title: 'Title 5',
+    content:
+      '<h3>Lorem ipsum.</h3><p>Mauris tortor eros, interdum at massa vitae, eleifend consequat justo. Donec ' +
+      'malesuada quam vel auctor vestibulum. Phasellus viverra, velit a malesuada suscipit, odio dui venenatis dui, ac ' +
+      'cursus purus nisi vel risus. Nullam convallis, dolor fringilla semper vulputate, massa nibh pellentesque orci, in ' +
+      'luctus elit tortor ut justo. Sed ac velit malesuada dolor iaculis facilisis. Nunc eget vehicula dolor, id dignissim t' +
+      'urpis. Etiam imperdiet vel risus sed rutrum. Donec ac viverra mi.</p>',
+    user_id: 2,
+    time: new Date()
+  },
+  {
+    id: 5,
+    title: 'Title 5',
+    content:
+      '<h3>Lorem ipsum.</h3><p>Mauris tortor eros, interdum at massa vitae, eleifend consequat justo. Donec ' +
+      'malesuada quam vel auctor vestibulum. Phasellus viverra, velit a malesuada suscipit, odio dui venenatis dui, ac ' +
+      'cursus purus nisi vel risus. Nullam convallis, dolor fringilla semper vulputate, massa nibh pellentesque orci, in ' +
+      'luctus elit tortor ut justo. Sed ac velit malesuada dolor iaculis facilisis. Nunc eget vehicula dolor, id dignissim t' +
+      'urpis. Etiam imperdiet vel risus sed rutrum. Donec ac viverra mi.</p>',
+    user_id: 2,
+    time: new Date()
+  },
+  {
+    id: 5,
+    title: 'Title 5',
+    content:
+      '<h3>Lorem ipsum.</h3><p>Mauris tortor eros, interdum at massa vitae, eleifend consequat justo. Donec ' +
+      'malesuada quam vel auctor vestibulum. Phasellus viverra, velit a malesuada suscipit, odio dui venenatis dui, ac ' +
+      'cursus purus nisi vel risus. Nullam convallis, dolor fringilla semper vulputate, massa nibh pellentesque orci, in ' +
+      'luctus elit tortor ut justo. Sed ac velit malesuada dolor iaculis facilisis. Nunc eget vehicula dolor, id dignissim t' +
+      'urpis. Etiam imperdiet vel risus sed rutrum. Donec ac viverra mi.</p>',
+    user_id: 2,
+    time: new Date()
+  },
 ];
--- src/app/classes/Post.ts
+++ src/app/classes/Post.ts
@@ -1,7 +1,8 @@
 export interface Post {
     id?: number,
+    user_id: number;
     content: string,
-  title: String;
+    title: String;
     comments?: any[],
     time?: Date
 }
