23a45d3382325d1aa9eb10f8502a42ef5dabf9a2
Timothy Harper <leeharperdev@gmail.com>
2021-11-08 08:59:02 -0600

    Update user.ts
    Updated: user.ts.
    -> Made more of the instance variables optional, as not all of this data is required for every user object call.

--- src/app/user.ts
+++ src/app/user.ts
@@ -2,10 +2,10 @@ export interface User {
     id?: number | undefined;
     firstName: string | undefined;
     lastName: string | undefined;
-    email: string | undefined;
+    email?: string | undefined;
     userName: string | undefined;
-    password: string | undefined;
-    gender: string | undefined;
-    branch: string | undefined;
-    birthdate: Date | undefined;
+    password?: string | undefined;
+    gender?: string | undefined;
+    branch?: string | undefined;
+    birthdate?: Date | undefined;
   }
