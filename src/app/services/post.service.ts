9057d5e8a9951ddab36fbf224d61e65e62592203
Jerry Zheng <jiaruizheng0714@gmail.com>
2021-11-10 23:50:57 -0500

    final v1.1

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -9,7 +9,7 @@ import {HttpClient, HttpHeaders} from "@angular/common/http";
   providedIn: 'root'
 })
 export class PostService {
-  baseUrl: string = `http://localhost:8000/`;
+  baseUrl: string = `/backend/`;
 
   posts : Post[];
   userToken! : string | null;
