ab4f958f3ab73fda978e4d5ef1f37bad2860ee69
Andrew Wolf <wolfpassion20@gmail.com>
2021-11-11 12:21:53 -0500

    Added documentation to NotificationService in the code

--- src/app/services/notification.service.ts
+++ src/app/services/notification.service.ts
@@ -8,7 +8,12 @@ export class NotificationService {
 
   constructor(private _toast: MatSnackBar) { }
 
-    //the toaster
+    /**
+     * Function: NotificationService.openToast(message: string, action: string)
+     * Purpose: displays messages to the user when they log in or register as to whether the registration was successful and whatnot
+     * @param message  The message for the notification to display to the user
+     * @param action   Really did not use this: if used, it was allow the user to click and dismiss the nofification bar
+     */
     openToast(message: string, action: string) {
       this._toast.open(message, action, {
         duration: 2500,
